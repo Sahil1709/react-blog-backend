efdeb8bdee6d088068c9c6ec4beadbe1c2590f09
Sahil <sahilofficial17@gmail.com>
2021-07-06 10:19:59 +0530

    changes

--- index.js
+++ index.js
@@ -1,43 +1,51 @@
-const express = require('express')
-const app = express()
-const dotenv = require('dotenv')
-dotenv.config()
-const mongoose = require('mongoose')
-const multer = require('multer')
-const authRoute = require('./routes/auth')
-const userRoute = require('./routes/users')
-const postRoute = require('./routes/posts')
-const categoryRoute = require('./routes/categories')
-const PORT = process.env.PORT || 5000
-app.use(express.json())
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoose = require("mongoose");
+const multer = require("multer");
+const path = require("path");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const PORT = process.env.PORT || 5000;
+app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/images")));
 
-mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
-}).then(console.log("Connected to mongoDB")).catch((err)=> console.log(err));
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(console.log("Connected to mongoDB"))
+  .catch((err) => console.log(err));
 
 const storage = multer.diskStorage({
-    destination:(req,file,cb)=>{
-        cb(null,"images")
-    },filename:(req,file,cb)=>{
-        cb(null,"req.body.name")
-    }
-})
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
 
-const upload = multer({storage:storage})
+const upload = multer({ storage: storage });
 
-app.post('/server/upload',upload.single("file"),(req,res)=>{
-    res.status(200).json("File has been uploaded");
-})
+app.post("/server/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
 
-app.get('/',(req,res)=>{
-    res.send("Hello world")
-})
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
 
-app.use('/server/auth',authRoute)
-app.use('/server/user',userRoute)
+app.use("/server/auth", authRoute);
+app.use("/server/user", userRoute);
 app.use("/server/posts", postRoute);
 app.use("/server/categories", categoryRoute);
 
-app.listen(PORT,() =>{
-    console.log("Server listening at http://127.0.0.1:"+PORT)
-})
+app.listen(PORT, () => {
+  console.log("Server listening at http://127.0.0.1:" + PORT);
+});
