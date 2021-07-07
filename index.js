f507e63ff88633f6a995a362a70124f60558d53d
Sahil <sahilofficial17@gmail.com>
2021-07-07 12:12:11 +0530

    fix

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ app.use(function (req, res, next) {
     "Access-Control-Allow-Headers",
     "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
   );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
   next();
 });
 
